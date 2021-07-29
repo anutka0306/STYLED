fee2129cab3b25dd97bf23d8a1aa1e980074b0a8
Anna Kotelnikova <anutka0306@mail.ru>
2021-07-29 17:57:21 +0300

    наши работы частично + last modified

---
+++ templates/elements/lastMod.php
@@ -0,0 +1,13 @@
+<?php
+$LastModified_unix = 1294844676;
+$LastModified = gmdate("D, d M Y H:i:s GMT", $LastModified_unix);
+$IfModifiedSince = false;
+if (isset($_ENV['HTTP_IF_MODIFIED_SINCE']))
+    $IfModifiedSince = strtotime(substr($_ENV['HTTP_IF_MODIFIED_SINCE'], 5));
+if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']))
+    $IfModifiedSince = strtotime(substr($_SERVER['HTTP_IF_MODIFIED_SINCE'], 5));
+if ($IfModifiedSince && $IfModifiedSince >= $LastModified_unix) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified');
+    exit;
+}
+header('Last-Modified: '. $LastModified);
